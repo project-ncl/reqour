9278fdc3637570240c6a04558903140a3a7bb19d
Adam Krídl <akridl@redhat.com>
2025-02-05 09:05:38 +0100

    [NCL-8972]: Authorize endpoints

---
+++ rest/src/test/java/org/jboss/pnc/reqour/rest/endpoints/TestConstants.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2024 Red Hat, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.pnc.reqour.rest.endpoints;
+
+public class TestConstants {
+
+    public static final String TEST_USER = "test-user";
+}
