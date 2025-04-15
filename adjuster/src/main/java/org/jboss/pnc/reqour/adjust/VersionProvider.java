f9db657011db6e942c43ff5df80039a16ce369f2
Adam Krídl <akridl@redhat.com>
2025-04-15 13:22:29 +0200

    Print adjuster version at startup

--- adjuster/src/main/java/org/jboss/pnc/reqour/adjust/VersionProvider.java
+++ adjuster/src/main/java/org/jboss/pnc/reqour/adjust/VersionProvider.java
@@ -4,15 +4,30 @@
  */
 package org.jboss.pnc.reqour.adjust;
 
+import io.quarkus.runtime.Startup;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.jboss.pnc.reqour.BuildInformationConstants;
+import org.jboss.pnc.reqour.runtime.UserLogger;
+import org.slf4j.Logger;
 import picocli.CommandLine;
+
+import java.util.Arrays;
 
 @ApplicationScoped
 public class VersionProvider implements CommandLine.IVersionProvider {
 
+    @Inject
+    @UserLogger
+    Logger userLogger;
+
+    @Startup
+    void printVersion() {
+        Arrays.stream(getVersion()).forEach(userLogger::info);
+    }
+
     @Override
-    public String[] getVersion() throws Exception {
+    public String[] getVersion() {
         return new String[] { "Reqour Adjuster", printVersionAttribute("Version", BuildInformationConstants.VERSION),
                 printVersionAttribute("Commit hash", BuildInformationConstants.COMMIT_HASH),
                 printVersionAttribute("Build time", BuildInformationConstants.BUILD_TIME) };
