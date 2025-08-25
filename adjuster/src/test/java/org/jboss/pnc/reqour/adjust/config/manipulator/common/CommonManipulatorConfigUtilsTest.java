9cf6ad2843111ecaf4c3edccde7f0ff870be6216
Adam Krídl <akridl@redhat.com>
2025-08-25 16:17:45 +0200

    [NCL-9243] Support Repour java parsing for 1.8.0

--- adjuster/src/test/java/org/jboss/pnc/reqour/adjust/config/manipulator/common/CommonManipulatorConfigUtilsTest.java
+++ adjuster/src/test/java/org/jboss/pnc/reqour/adjust/config/manipulator/common/CommonManipulatorConfigUtilsTest.java
@@ -5,14 +5,17 @@
 package org.jboss.pnc.reqour.adjust.config.manipulator.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.jboss.pnc.api.constants.BuildConfigurationParameterKeys;
 import org.jboss.pnc.api.reqour.dto.AdjustRequest;
+import org.jboss.pnc.reqour.adjust.exception.AdjusterException;
 import org.jboss.pnc.reqour.adjust.model.LocationAndRemainingAlignmentParameters;
 import org.jboss.pnc.reqour.adjust.model.UserSpecifiedAlignmentParameters;
 import org.junit.jupiter.api.Test;
@@ -124,5 +127,30 @@ class CommonManipulatorConfigUtilsTest {
                         "target");
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void getJavaLocation_noJavaVersionOverride_defaultIsUsed() {
+        assertThat(CommonManipulatorConfigUtils.getJavaLocation(Collections.emptyList())).isEqualTo(
+                CommonManipulatorConfigUtils.getJavaOfVersion(CommonManipulatorConfigUtils.DEFAULT_JAVA_VERSION));
+    }
+
+    @Test
+    void getJavaLocation_java17VersionOverrideGiven_overrideIsUsed() {
+        assertThat(CommonManipulatorConfigUtils.getJavaLocation(List.of("-DRepour_Java=17")))
+                .isEqualTo(CommonManipulatorConfigUtils.getJavaOfVersion("17"));
+    }
+
+    @Test
+    void getJavaLocation_java8VersionOverrideGiven_overrideIsUsed() {
+        assertThat(CommonManipulatorConfigUtils.getJavaLocation(List.of("-DRepour_Java=1.8.0")))
+                .isEqualTo(CommonManipulatorConfigUtils.getJavaOfVersion("1.8.0"));
+    }
+
+    @Test
+    void getJavaLocation_invalidJavaVersionOverrideGiven_throwsException() {
+        assertThatThrownBy(() -> CommonManipulatorConfigUtils.getJavaLocation(List.of("-DRepour_Java=invalid")))
+                .isInstanceOf(AdjusterException.class)
+                .hasMessage("Invalid Java version 'invalid' provided.");
     }
 }
