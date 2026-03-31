ae3a26b77b277db4ac013ff3fca56c2e3eb7c247
Dustin Kut Moy Cheung <dcheung@redhat.com>
2026-03-31 14:24:48 -0400

    Adapt to new interface for PNCClientAuth

--- adjuster/src/test/java/org/jboss/pnc/reqour/adjust/common/PNCClientAuthMock.java
+++ adjuster/src/test/java/org/jboss/pnc/reqour/adjust/common/PNCClientAuthMock.java
@@ -34,4 +34,9 @@ public class PNCClientAuthMock implements PNCClientAuth {
     public LDAPCredentials getLDAPCredentials() throws IOException {
         return new LDAPCredentials("user", "password");
     }
+
+    @Override
+    public ClientAuthType getConfiguredType() {
+        return ClientAuthType.OIDC;
+    }
 }
