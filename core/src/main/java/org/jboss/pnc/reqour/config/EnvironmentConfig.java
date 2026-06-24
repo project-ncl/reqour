e00fb480e5b931ba7a45f26adca36464cdcf37ff
Adam Krídl <akridl@redhat.com>
2026-06-24 08:08:47 +0200

    Reapply "[NCLSUP-1441] Compute name and version when GME is turned off"
    This reverts commit ae6e5d7e5b328fb547fefe8e67d119e2aa1e0f7d.

--- core/src/main/java/org/jboss/pnc/reqour/config/EnvironmentConfig.java
+++ core/src/main/java/org/jboss/pnc/reqour/config/EnvironmentConfig.java
@@ -9,7 +9,15 @@ import io.smallrye.config.WithName;
 public interface EnvironmentConfig {
 
     String HOME_ENV_VARIABLE = "HOME";
+    String PATH_ENV_VARIABLE = "PATH";
+    String JAVA_HOME_ENV_VARIABLE = "JAVA_HOME";
 
     @WithName(HOME_ENV_VARIABLE)
     String home();
+
+    @WithName(PATH_ENV_VARIABLE)
+    String path();
+
+    @WithName(JAVA_HOME_ENV_VARIABLE)
+    String javaHome();
 }
