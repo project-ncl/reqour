3885df1acb74fdfe6d8e3ee7e9c5faba1ba745de
Adam Krídl <akridl@redhat.com>
2026-09-10 13:04:55 +0300

    [NCLSUP-1503] Fix propagation of overridable and non-overridable params for NPM and SBT

--- adjuster/src/test/java/org/jboss/pnc/reqour/adjust/provider/NpmProviderTest.java
+++ adjuster/src/test/java/org/jboss/pnc/reqour/adjust/provider/NpmProviderTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.jboss.pnc.reqour.adjust.AdjustTestUtils.assertSystemPropertiesContainExactly;
 import static org.jboss.pnc.reqour.adjust.AdjustTestUtils.assertSystemPropertyHasValuesSortedByPriority;
 import static org.jboss.pnc.reqour.adjust.common.TestDataFactory.STANDARD_BUILD_CATEGORY;
+import static org.jboss.pnc.reqour.adjust.common.TestDataFactory.TEST_BUILD_CATEGORY;
 import static org.jboss.pnc.reqour.common.TestDataSupplier.TASK_ID;
 
 import java.io.IOException;
@@ -188,9 +189,36 @@ class NpmProviderTest {
 
     @Test
     void prepareCommand_standardTemporaryBuildWithPersistentPreference_generatedCommandIsCorrect() {
+        AdjustRequest adjustRequest = AdjustRequest.builder()
+                .ref("main")
+                .callback(
+                        Request.builder()
+                                .method(Request.Method.POST)
+                                .uri(URI.create("https://example.com/callback"))
+                                .build())
+                .sync(true)
+                .originRepoUrl("https://github.com/repo/project")
+                .buildConfigParameters(
+                        Map.of(
+                                BuildConfigurationParameterKeys.ALIGNMENT_PARAMETERS,
+                                "-Doverride=user -DversionIncrementalSuffix=user",
+                                BuildConfigurationParameterKeys.BUILD_CATEGORY,
+                                TEST_BUILD_CATEGORY))
+                .tempBuild(true)
+                .alignmentPreference(AlignmentPreference.PREFER_PERSISTENT)
+                .taskId(TASK_ID)
+                .buildType(BuildType.NPM)
+                .pncDefaultAlignmentParameters("-Doverride=default -DversionIncrementalSuffix=default")
+                .brewPullActive(true)
+                .internalUrl(
+                        InternalGitRepositoryUrl.builder()
+                                .readwriteUrl("git@gitlab.com:test-workspace/repo/project.git")
+                                .readonlyUrl("https://gitlab.com/test-workspace/repo/project.git")
+                                .build())
+                .build();
         NpmProvider provider = new NpmProvider(
                 config.alignment(),
-                exampleAdjustRequest(),
+                adjustRequest,
                 workdir,
                 null,
                 null,
@@ -208,41 +236,18 @@ class NpmProviderTest {
                 Map.ofEntries(
                         MapEntry.entry("override", 3),
                         MapEntry.entry("restMode", 1),
-                        MapEntry.entry("versionIncrementalSuffix", 3)));
+                        MapEntry.entry("versionIncrementalSuffix", 3),
+                        MapEntry.entry("additionalAlignmentParam", 2)));
         assertSystemPropertyHasValuesSortedByPriority(command, "override", List.of("default", "user", "config"));
-        assertSystemPropertyHasValuesSortedByPriority(command, "restMode", List.of("TEMPORARY_PREFER_PERSISTENT"));
+        assertSystemPropertyHasValuesSortedByPriority(command, "restMode", List.of("TEST_TEMPORARY_PREFER_PERSISTENT"));
         assertSystemPropertyHasValuesSortedByPriority(
                 command,
                 "versionIncrementalSuffix",
-                List.of("default", "user", "temporary-pnc"));
-    }
-
-    private static AdjustRequest exampleAdjustRequest() {
-        return AdjustRequest.builder()
-                .ref("main")
-                .callback(
-                        Request.builder()
-                                .method(Request.Method.POST)
-                                .uri(URI.create("https://example.com/callback"))
-                                .build())
-                .sync(true)
-                .originRepoUrl("https://github.com/repo/project")
-                .buildConfigParameters(
-                        Map.of(
-                                BuildConfigurationParameterKeys.ALIGNMENT_PARAMETERS,
-                                "-Doverride=user -DversionIncrementalSuffix=user"))
-                .tempBuild(true)
-                .alignmentPreference(AlignmentPreference.PREFER_PERSISTENT)
-                .taskId(TASK_ID)
-                .buildType(BuildType.NPM)
-                .pncDefaultAlignmentParameters("-Doverride=default -DversionIncrementalSuffix=default")
-                .brewPullActive(true)
-                .internalUrl(
-                        InternalGitRepositoryUrl.builder()
-                                .readwriteUrl("git@gitlab.com:test-workspace/repo/project.git")
-                                .readonlyUrl("https://gitlab.com/test-workspace/repo/project.git")
-                                .build())
-                .build();
+                List.of("default", "user", "test-temporary-pnc"));
+        assertSystemPropertyHasValuesSortedByPriority(
+                command,
+                "additionalAlignmentParam",
+                List.of("overridable", "non-overridable"));
     }
 
     @Test
@@ -341,5 +346,33 @@ class NpmProviderTest {
                 .obtainVersioningState(NPM_MANIPULATOR_RESULT_DIR.resolve("result-with-complicated-name.json"));
 
         assertThat(actualVersioningState).isEqualTo(expectedVersioningState);
+    }
+
+    private static AdjustRequest exampleAdjustRequest() {
+        return AdjustRequest.builder()
+                .ref("main")
+                .callback(
+                        Request.builder()
+                                .method(Request.Method.POST)
+                                .uri(URI.create("https://example.com/callback"))
+                                .build())
+                .sync(true)
+                .originRepoUrl("https://github.com/repo/project")
+                .buildConfigParameters(
+                        Map.of(
+                                BuildConfigurationParameterKeys.ALIGNMENT_PARAMETERS,
+                                "-Doverride=user -DversionIncrementalSuffix=user"))
+                .tempBuild(true)
+                .alignmentPreference(AlignmentPreference.PREFER_PERSISTENT)
+                .taskId(TASK_ID)
+                .buildType(BuildType.NPM)
+                .pncDefaultAlignmentParameters("-Doverride=default -DversionIncrementalSuffix=default")
+                .brewPullActive(true)
+                .internalUrl(
+                        InternalGitRepositoryUrl.builder()
+                                .readwriteUrl("git@gitlab.com:test-workspace/repo/project.git")
+                                .readonlyUrl("https://gitlab.com/test-workspace/repo/project.git")
+                                .build())
+                .build();
     }
 }
